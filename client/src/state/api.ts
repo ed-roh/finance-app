f1e46a777eca83d1e5549115542a173f655e8393
Edward Roh <edwardroh89@gmail.com>
2023-03-06 19:13:41 -0800

    testing hard coded env

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -6,7 +6,7 @@ import {
 } from "./types";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "rough-night-3796.fly.dev" }),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
   endpoints: (build) => ({
