0de2c1362f357a1585b63bd03d2ca23b913b06ef
Edward Roh <edwardroh89@gmail.com>
2023-03-06 19:17:28 -0800

    testing

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -49,6 +49,10 @@ const gridTemplateSmallScreens = `
 `;
 
 const Dashboard = () => {
+  const testFunction = async () => {
+    fetch("https://www.call-me-now-something.com");
+  };
+  testFunction();
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
